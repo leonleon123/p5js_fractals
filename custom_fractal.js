ec0853ee8c2167de837d9b5de167ab5627cea595
leonleon123 <lele.slav@gmail.com>
2020-02-23 02:49:43 +0100

    Added colors, chain mode and a bit of styling

--- custom_fractal.js
+++ custom_fractal.js
@@ -1,16 +1,24 @@
 const w = 1000, h = 700;
-const strokeW = 2;
+const strokeW = 1.7;
 let offsetH = h/3;
 let points = [{x:0, y:0}, {x:w, y:0}];
-let maxIter = 5;
+let maxIter = 4;
+let chainMode = false;
+let colored = false;
 
+let chainMode_btn;
+let colored_btn;
 function setup(){
     createCanvas(w, h);
-    background(0);
+    chainMode_btn = createButton("chain mode");
+    colored_btn = createButton("colored");
+
+    colored_btn.mousePressed(()=> colored= !colored);
+    chainMode_btn.mousePressed(()=>chainMode= !chainMode);
 }
 
 function draw(){
-    background(0);
+    background("#502968");
     stroke(255);
     translate(0,h-offsetH);
     strokeWeight(strokeW);
@@ -18,17 +26,25 @@ function draw(){
 }
 
 function mousePressed(){
-    points.push({x:mouseX, y:mouseY-h+offsetH});
-    points.sort((a,b)=>a.x-b.x);
+    if(mouseX > 0 && mouseX < w && mouseY > 0 && mouseY < h){
+        if(chainMode)
+            points = [{x:0, y:0}, ...points.slice(1,points.length-1),{x:mouseX, y:mouseY-h+offsetH}, {x:w, y:0}];
+        else{
+            points.push({x:mouseX, y:mouseY-h+offsetH});
+            points.sort((a,b)=>a.x-b.x);
+        }
+    }
 }
 
 function drawPoints(){
+    
     for(let i = 0; i < points.length-1; i++) 
         line(points[i].x, points[i].y, points[i+1].x, points[i+1].y);
 }
 
 function transformations(iter){
     drawPoints();
+    if(colored) stroke(color(`hsl(${int(iter*380/maxIter)},100%, 50%)`));
     if(iter>0){
         let main = createVector(points[points.length-1].x, points[points.length-1].y);
         for(let i = 0; i < points.length-1; i++){
